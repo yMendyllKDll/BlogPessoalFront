8fe889f03c9664757301b1f1e55930a4067559ec
Anderson <and_mendescosta@hotmail.com>
2022-05-09 14:19:39 -0300

    Implementação do get e post de temas

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,23 +1,56 @@
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
-import { Component, OnInit } from '@angular/core';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
-  selector: 'app-tema',
+  selector: 'app-temas',
   templateUrl: './tema.component.html',
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(){
+
     if(environment.token ==''){
-      alert('Sessão expirada, faça login novamente.')
+      alert('Sessão expirada, faça login para continuar')
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTema()
+
+
   }
 
+  findAllTema(){
+    this.temaService.getAllTema().subscribe((resp:Tema[]) =>{
+      this.listaTema = resp
+      console.log(this.listaTema)
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe({
+      next: (resp:Tema) =>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.tema = new Tema()
+      this.findAllTema()
+      },
+      error: (erro) => {
+        if(erro.status == 400){
+          alert('Tema não pode ser cadastrado pois já existe um tema com está descrição');
+        }
+      },
+    })
+  }
 }
